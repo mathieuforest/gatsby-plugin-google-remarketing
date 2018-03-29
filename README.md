2034cde0795e37767b3b3c2d80b8b121224ccb05
mathieuforest <mathieu.forest@exob2b.com>
2018-03-29 17:07:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# https://github.com/mathieuforest/gatsby-plugin-google-remarketing
+
+Easily add Google Remarketing tag to your Gatsby site.
+
+## Install
+
+`npm install --save gatsby-plugin-google-remarketing`
+
+## How to use
+
+```javascript
+// In your gatsby-config.js
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-remarketing`,
+      options: {
+        google_conversion_id: "YOUR_GOOGLE_CONVERSION_ID",
+        google_custom_params: "YOUR_GOOGLE_CUSTOM_PARAMS",
+        google_remarketing_only: "IS_REMARKETING_ONLY",
+        // Puts tracking script in the head instead of the body
+        head: false,
+      },
+    },
+  ],
+};
+```
---
+++ src/gatsby-ssr.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }, pluginOptions) => {
+  if (process.env.NODE_ENV === `production`) {
+    const setComponents = pluginOptions.head ? setHeadComponents : setPostBodyComponents
+    return setComponents([
+      <script type="text/javascript" key={`gatsby-plugin-google-remarketing-variables`} dangerouslySetInnerHTML={{
+        __html: `
+            /* <![CDATA[ */
+            var google_conversion_id = ${pluginOptions.google_conversion_id}
+            var google_custom_params = window.google_tag_params;
+            var google_remarketing_only = ${pluginOptions.google_remarketing_only}
+            /* ]]> */
+        `
+      }}>
+      </script>,
+      <script key={`gatsby-plugin-google-remarketing-script`} type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>,
+      <noscript key={`gatsby-plugin-google-remarketing-no-script`} dangerouslySetInnerHTML={{
+        __html: `
+          <div style="display: inline;">
+            <img height="1" width="1" style="borderStyle: none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/${pluginOptions.google_conversion_id}/?guid=ON&amp;script=0"/>
+          </div>
+        `
+      }}>
+      </noscript>
+    ])
+  }
+  return null
+}
